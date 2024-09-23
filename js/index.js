2e312e1cd072022e0f804ab5ed53f584d1ebbc8e
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-09-23 19:11:20 +0600

    feni & quota card done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // changing the pages
-document.getElementById('btn-toggle').addEventListener('click', function() {
+document.getElementById('btn-toggle').addEventListener('click', function(event) {
+    event.preventDefault();
     if (window.location.href.includes('blog.html')) {
         window.location.href = 'index.html';
     } else {
@@ -22,11 +23,15 @@ let sum = 0;
 let updatedBalance = 0;
 
 // input fields buttons
-const buttonNoakhali = document.getElementById('btn-noakhali');
-buttonNoakhali.addEventListener('click', function(){
 
+// 1st card
+const buttonNoakhali = document.getElementById('btn-noakhali');
+buttonNoakhali.addEventListener('click', function(event){
+    event.preventDefault();
     const N_Donation = parseFloat(document.getElementById('noakhali-donate-field').value);
     const noakhaliTotalDonate = document.getElementById('noakhali-amount');
+    const noakhaliTotalDonateValue = parseFloat(noakhaliTotalDonate.innerText);
+    sum = noakhaliTotalDonateValue;
 
     sum+=N_Donation;
     noakhaliTotalDonate.innerText = sum;
@@ -39,6 +44,60 @@ buttonNoakhali.addEventListener('click', function(){
     historyItem.className = 'space-y-4 max-w-6xl container mx-auto p-8 border rounded-xl mb-6';
     historyItem.innerHTML = `
     <h3 class="text-xl font-bold text-head-color">${N_Donation} Taka is Donated for famine-2024 at Noakhali, Bangladesh</h3>
+    <p>Date: ${new Date().toString()}</p>
+    `
+
+    // const historyContainer = document.getElementById('add-history');
+    historySection.appendChild(historyItem);
+});
+
+// 2nd card
+const buttonFeni = document.getElementById('btn-feni');
+buttonFeni.addEventListener('click', function(event){
+    event.preventDefault();
+    const F_Donation = parseFloat(document.getElementById('feni-donate-field').value);
+    const feniTotalDonate = document.getElementById('feni-amount');
+    const feniTotalDonateValue = parseFloat(feniTotalDonate.innerText);
+    sum = feniTotalDonateValue;
+
+    sum+=F_Donation;
+    feniTotalDonate.innerText = sum;
+
+    const totalBalanceValue = parseFloat(totalBalance.innerText);
+    updatedBalance = totalBalanceValue - F_Donation;
+    totalBalance.innerText = updatedBalance;
+    
+    const historyItem = document.createElement('div');
+    historyItem.className = 'space-y-4 max-w-6xl container mx-auto p-8 border rounded-xl mb-6';
+    historyItem.innerHTML = `
+    <h3 class="text-xl font-bold text-head-color">${F_Donation} Taka is Donated for Flood Relief in Feni, Bangladesh</h3>
+    <p>Date: ${new Date().toString()}</p>
+    `
+
+    // const historyContainer = document.getElementById('add-history');
+    historySection.appendChild(historyItem);
+});
+
+// 3rd card
+const buttonQuota = document.getElementById('btn-quota');
+buttonQuota.addEventListener('click', function(event){
+    event.preventDefault();
+    const Q_Donation = parseFloat(document.getElementById('quota-donate-field').value);
+    const quotaTotalDonate = document.getElementById('quota-amount');
+    const quotaTotalDonateValue = parseFloat(quotaTotalDonate.innerText);
+    sum = quotaTotalDonateValue;
+
+    sum+=Q_Donation;
+    quotaTotalDonate.innerText = sum;
+
+    const totalBalanceValue = parseFloat(totalBalance.innerText);
+    updatedBalance = totalBalanceValue - Q_Donation;
+    totalBalance.innerText = updatedBalance;
+    
+    const historyItem = document.createElement('div');
+    historyItem.className = 'space-y-4 max-w-6xl container mx-auto p-8 border rounded-xl mb-6';
+    historyItem.innerHTML = `
+    <h3 class="text-xl font-bold text-head-color">${Q_Donation} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h3>
     <p>Date: ${new Date().toString()}</p>
     `
 
